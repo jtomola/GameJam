225471e2f0ac69b40a13a3a0668c57ee2a24a66d
jtomola <jamestomola@gmail.com>
2014-10-18 17:06:29 -0500

    Configured triggers for blow on all 4 players. Turned player into a prefab. Added a meter that goes down for blowing (not a visible meter).

--- Assets/Scripts/BlowScript.cs
+++ Assets/Scripts/BlowScript.cs
@@ -3,21 +3,31 @@ using System.Collections;
 
 public class BlowScript : MonoBehaviour {
     public float maxForce;
+    public int playerNumber;
+    private string inputString;
+    private bool isFiring;
+    public float delay = 0.0f;
+    public float drainSpeed = 1.0f;
+    public float refillSpeed = 1.0f;
+    private float maxMeter = 100.0f;
+    public float currMeter = 100.0f;
+    private bool isRegen = false;
 
-    /*
-    void OnTriggerStay(Collider collider)
+    void OnTriggerStay2D(Collider2D collider)
     {
-        Debug.Log("On Trigger Stay");
+        //Debug.Log("On Trigger Stay");
         GameObject obj = collider.gameObject;
 
-        if (obj.CompareTag("SoulTag") == true)
+        if (obj.CompareTag("SoulTag") == true && this.isFiring)
         {
-            Vector2 force = maxForce * transform.forward;
-            obj.rigidbody2D.AddForce(force, ForceMode2D.Force);
+            Vector2 diffPos = obj.transform.position - this.transform.position;
+            diffPos.Normalize();
+            Vector2 force = diffPos * this.maxForce;
+            obj.rigidbody2D.AddForce(force, ForceMode2D.Impulse);
         }
     }
-    */
 
+    /*
     void OnTriggerEnter2D(Collider2D collider)
     {
         GameObject obj = collider.gameObject;
@@ -30,13 +40,62 @@ public class BlowScript : MonoBehaviour {
             obj.rigidbody2D.AddForce(force, ForceMode2D.Impulse);
         }
     }
+    */
 
 	// Use this for initialization
 	void Start () {
+        switch (playerNumber)
+        {
+            case 1:
+                inputString = "Fire1";
+                break;
+            case 2:
+                inputString = "Fire2";
+                break;
+            case 3:
+                inputString = "Fire3";
+                break;
+            case 4:
+                inputString = "Fire4";
+                break;
+            default:
+                inputString = "Fire1";
+                break;
+        }
+        this.isFiring = false;
+        this.isRegen = true;
+        this.currMeter = this.maxMeter;
 	}
+
+    private void StartRegen()
+    {
+        this.currMeter = 0.5f * this.maxMeter;
+        this.isRegen = true;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+        float strength = Input.GetAxis(inputString);
+        if (Mathf.Abs(strength) > 0.1f && this.isRegen)
+        {
+            this.isFiring = true;
+            this.currMeter -= this.drainSpeed * Time.deltaTime;
+            if (this.currMeter < 0.0f)
+            {
+                this.isFiring = false;
+                this.isRegen = false;
+                Invoke("StartRegen", this.delay);
+            }
+        }
+        else
+        {
+            this.isFiring = false;
+            if (this.isRegen)
+            {
+                this.currMeter += this.drainSpeed * Time.deltaTime;
+                if (this.currMeter > this.maxMeter)
+                    this.currMeter = this.maxMeter;
+            }
+        }
 	}
 }
