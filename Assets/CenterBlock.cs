69f720a853b2b029c9e00799e8269397095265d8
Josh <joshbrinson@users.noreply.github.com>
2014-10-18 17:35:27 -0500

    I am the best dev ever

---
+++ Assets/CenterBlock.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CenterBlock : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void FixedUpdate()
+    {
+        this.rigidbody2D.AddTorque(0.02f);
+    }
+}
