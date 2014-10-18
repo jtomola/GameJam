fe001d459422ea27146163da43c6aad0d5284bb3
Josh <joshbrinson@users.noreply.github.com>
2014-10-18 13:44:17 -0500

    Prototype health loss on collision

---
+++ Assets/PlayerCollisions.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollisions : MonoBehaviour {
+
+    public int Health;
+    public TextMesh textMesh;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        
+	}
+
+    void FixedUpdate()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "SoulTag")
+        {
+            Health--;
+            Debug.Log("Collision code");
+            this.GetComponent<TextMesh>().text = "Health: " + Health.ToString();
+        }
+    }
+}
