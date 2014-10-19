925389c7789a296f17fb7464537cc4b2f96e44c0
Josh <joshbrinson@users.noreply.github.com>
2014-10-19 12:40:07 -0500

    Big UI Refactor

---
+++ Assets/GameTimeUpdate.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameTimeUpdate : MonoBehaviour {
+
+    public TimerScript levelTimer;
+
+    private float time;
+
+    public GUIText rightTimerText;
+    public GUIText leftTimerText;
+
+    public GUIText winMessage;
+    public ScoreKeeper scoreKeeper;
+
+	// Use this for initialization
+	void Start () {
+
+        winMessage.enabled = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        time = levelTimer.GetTime();
+
+        rightTimerText.text = "Time: " + time.ToString() + "s";
+        leftTimerText.text = "Time: " + time.ToString() + "s";
+
+        if (time <= 0)
+        {
+            // Win Message
+            if (scoreKeeper.purpleGoal.GetScore() > scoreKeeper.turqouiseGoal.GetScore())
+            {
+                winMessage.text = "Purple Team Wins!!";
+            }
+            else if (scoreKeeper.purpleGoal.GetScore() < scoreKeeper.turqouiseGoal.GetScore())
+            {
+                winMessage.text = "Turqouise Team Wins!!";
+            }
+            else
+            {
+                winMessage.text = "Tie Game!!";
+            }
+
+            winMessage.enabled = true;
+
+            scoreKeeper.purpleGoal.gameObject.SetActive(false);
+            scoreKeeper.turqouiseGoal.gameObject.SetActive(false);
+        }
+	
+	}
+}
---
+++ Assets/ScoreKeeper.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreKeeper : MonoBehaviour {
+
+    public Goal purpleGoal;
+    public Goal turqouiseGoal;
+
+    public GUIText purpleScoreText;
+    public GUIText turqouiseScoreText;
+
+    private int PurpleScore;
+    private int TurqouiseScore;
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        PurpleScore = purpleGoal.GetScore();
+        TurqouiseScore = turqouiseGoal.GetScore();
+
+        purpleScoreText.text = purpleGoal.GetTeam() + " Score: " + PurpleScore.ToString();
+        turqouiseScoreText.text = turqouiseGoal.GetTeam() + " Score: " + TurqouiseScore.ToString();
+
+	}
+}
